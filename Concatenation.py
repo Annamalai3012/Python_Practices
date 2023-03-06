b7f1d291c0e0a4d1b6da604bb27a983283043535
Annamalai3012 <49242730+Annamalai3012@users.noreply.github.com>
2023-03-06 20:36:04 +0000

    Add files via upload

---
+++ Concatenation.py
@@ -0,0 +1,6 @@
+#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+
+print("Welcome to Band Name Generator")
+city_name = input("What's the name of the city you grew up in?\n")
+pet= input("What's your pet name?\n")
+print("Your band name could be " + city_name + " " + pet)
---
+++ printing.py
@@ -0,0 +1,11 @@
+#Write your code below this line 👇
+print("Day 1 - Python print Function")
+print('The function is declared like this:')
+print("print('what to print')")
+
+
+
+
+
+
+
